84c18d40ba9d6f34f167204a9e3ee51700083b2d
emiliogozo <emilio.gozo@obf.ateneo.edu>
2016-05-17 13:54:22 +0800

    fixed initialization of empty array

--- www/js/services.js
+++ www/js/services.js
@@ -22,7 +22,7 @@ angular.module('conFusion.services', ['ngResource'])
 
 .factory('favoriteFactory', ['$resource', '$localStorage', 'baseURL', function ($resource, $localStorage, baseURL) {
       var favFac = {};
-      var favorites = $localStorage.getObject('favorites','{}');
+      var favorites = $localStorage.getObject('favorites','[]');
 
       favFac.addToFavorites = function (index) {
         for (var i = 0; i < favorites.length; i++) {
